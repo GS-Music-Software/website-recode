b65c692e71c9457e7de3b68017886639a31ea183
sagevk <sagevk@users.noreply.github.com>
2026-02-26 17:21:54 -0500

    change some stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  server: { port: 5000, host: '0.0.0.0' },
   vite: {
     plugins: [tailwindcss()]
   }
